bc97fcf8aa9e95f4d279ed6a0f8ee2377adb8b16
ivantxo <ivan.ramirezf@gmail.com>
2020-02-22 02:27:20 +1000

    - Instantiated Vue, - Added data back to the template

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: 'My apartment',
+    address: '34 Osterley Road, Carina Heights, Brisbane, Australia',
+    about: 'This is a nice townhouse we are thinking to buy'
+  }
+});
