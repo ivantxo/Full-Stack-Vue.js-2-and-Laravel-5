0b821741ced904478dfd563848c0acb26ade6571
ivantxo <ivan.ramirezf@gmail.com>
2020-02-22 02:30:39 +1000

    Added mock data.js

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,8 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
-    title: 'My apartment',
-    address: '34 Osterley Road, Carina Heights, Brisbane, Australia',
-    about: 'This is a nice townhouse we are thinking to buy'
+    title: sample.title,
+    address: sample.address,
+    about: sample.about
   }
 });
